b505e5832a08e0e128424e3f99771a4de69db16b
mjrode <michaelrode44@gmail.com>
2016-04-14 16:31:30 -0400

    Added extra tests for the ExpectationCollection class Needed additional tests before refactoring

--- test/expectation_collection_test.rb
+++ test/expectation_collection_test.rb
@@ -16,6 +16,30 @@ class ExpectationCollectionTest < Minitest::Test
     assert_raises (MissingParameterError) do
       ex.verify(params)
     end
-
   end
+
+  should "parameters hash should pass when one parameter is not nil" do 
+    ex = ExpectationCollection.new(:name, :age, :id)
+    params = {name: nil, age: 55}
+    assert_nothing_raised do 
+      ex.verify(params)
+    end
+  end
+
+  should "parameters hash should pass even if some parameters are missing" do 
+    ex = ExpectationCollection.new(:name, :age, :id)
+    params = {name: "Michael"}
+    assert_nothing_raised do 
+      ex.verify(params)
+    end
+  end
+
+  should "parameters hash should fail if no parameters are passed in" do 
+    ex = ExpectationCollection.new(:name, :age, :id)
+    params = {}
+    assert_raises (MissingParameterError) do 
+      ex.verify(params)
+    end
+  end
+
 end
